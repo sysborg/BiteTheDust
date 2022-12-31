6e8aafa8eaecc118009e4390d939de5cbd05fbc1
Anderson Arruda <andmarruda@gmail.com>
2022-12-30 22:40:12 -0300

    added test for grayscale

---
+++ test/srcset/grayscaleTest.php
@@ -0,0 +1,9 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once __DIR__ . '/../src/autoloader.php';
+    $btd = new \sysborg\btd(__DIR__. '/1669982787114.jpg');
+    $btd->grayscale()->save(__DIR__. 'grayscale.jpg', 'jpg');
+?>
